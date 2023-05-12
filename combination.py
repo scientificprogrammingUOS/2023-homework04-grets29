8c2828f7a305394e0304b533a131b09d47b8703c
Greta Bramow <gsb@mini-von-greta.speedport.ip>
2023-05-12 18:23:13 +0200

    Homework 4 done

--- combination.py
+++ combination.py
@@ -2,12 +2,14 @@ import numpy as np
 
 # implement your function to combine two numpy arrays 
 
-def combination():
-    # delete the NotImplementedError when you write your function.
-    raise NotImplementedError
-
+def combination(array1, array2, axis=0):
+    array1, array2 = array1.squeeze(), array2.squeeze()
+    try:
+        return np.concatenate((array1, array2),axis=axis)
+    except ValueError as e:
+        raise ValueError(f"Cannot combine arrays. Array1 shape: {array1.shape} Array2 shape: {array2.shape}, Axis: {axis}. Original error: {str(e)}") from None
 
 if __name__ == "__main__":
     # use this for your own testing!
-
-    pass
+    a = combination(np.array([22,3]),np.arange(5))
+    print(a)
--- strange_pattern.py
+++ strange_pattern.py
@@ -2,12 +2,16 @@ import numpy as np
 
 # implement the function strange pattern
 
-def strange_pattern():
-    # delete the NotImplementedError when you write your function.
-    raise NotImplementedError
+def strange_pattern(tuple):
+    n, m = tuple
+    array = np.full((n,m),False)
+    array[0::3, 0::3] = True
+    array[2::3, 1::3] = True
+    array[1::3, 2::3] = True
 
+    return array
 
 if __name__ == "__main__":
     # use this for your own testing!
-
-    pass
+    print(strange_pattern((8,8)))
+    
